bab1fa2618eb0a9163148ba0da2bf7a9068f157b
Your Name <you@example.com>
2020-03-12 17:11:15 -0300

    testando para fazer S.A

---
+++ functions.js
@@ -0,0 +1,8 @@
+function chamarIframe(){
+    document.getElementById("iframe").style.display = "block";
+    document.getElementById("iframe2").style.display = "none";
+}
+function chamarIframe2(){
+    document.getElementById("iframe2").style.display = "block";
+    document.getElementById("iframe").style.display = "none";
+}
